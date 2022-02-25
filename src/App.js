c82ab8600599afb071ba512a15d5dd680d0bea4b
USERNAME <EMAIL>
2022-02-25 06:19:08 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+                Hi guys
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
